cde94539ccc88dc98af6521c2bb146bea6328062
Mohit Lahoti <engtechno25@gmail.com>
2024-11-17 16:02:41 +0530

    Separating appbar logic from main screen to custom widget

--- lib/home/screen/main.dart
+++ lib/home/screen/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tchat_frontend/home/widget/drawer.dart';
+import 'package:tchat_frontend/home/widget/call_tab.dart';
 import 'package:tchat_frontend/home/widget/messages_tab.dart';
 
 class HomeMainScreen extends StatefulWidget {
@@ -16,16 +16,6 @@ class _HomeMainScreen extends State<HomeMainScreen> {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.primary,
-          centerTitle: true,
-          title: Text("Conversations",
-          style: TextStyle(color: Theme.of(context).colorScheme.surface, fontSize: 18, fontWeight: FontWeight.w600),),
-          iconTheme: IconThemeData(color: Theme.of(context).colorScheme.surface),
-        ),
-        drawer: SizedBox(
-          width: 270,
-          child: MainDrawer()),
         body: SafeArea(
           child: Column(
             children: [
@@ -34,7 +24,7 @@ class _HomeMainScreen extends State<HomeMainScreen> {
                   children: [
                     MessagesTab(),
                     Center(child: Text('Tab 2 Content')),
-                    Center(child: Text('Tab 3 Content')),
+                    CallScreen(),
                   ],
                 ),
               ),
