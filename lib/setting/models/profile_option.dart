f30cfb1b2391f7ab6c21927bba7c2ded53c2c0d9
Mohit Lahoti <engtechno25@gmail.com>
2024-11-21 00:02:35 +0530

    Created profile screen option model

---
+++ lib/setting/models/profile_option.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProfileOption {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+
+  ProfileOption({required this.icon, required this.title, required this.subtitle});
+}
