4769fee4ae94266b7d74df78484479a5be841ba5
Mohit Lahoti <engtechno25@gmail.com>
2025-02-08 18:24:13 +0530

    Adding figma svg icons of menu tab bar

--- lib/home/screen/dashboard.dart
+++ lib/home/screen/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:tchat_frontend/home/screen/call_tab.dart';
 import 'package:tchat_frontend/home/screen/communities_tab.dart';
 import 'package:tchat_frontend/home/screen/messages_tab.dart';
@@ -38,23 +39,33 @@ class _HomeMainScreen extends State<HomeMainScreen> {
             labelColor: Theme.of(context).colorScheme.primary,
             unselectedLabelColor: Colors.grey,
             indicatorColor: Theme.of(context).colorScheme.primary,
-            tabs: const [
-              Tab(
-                icon: Icon(Icons.message),
-                text: 'Chats',
-              ),
-              Tab(
-                icon: Icon(Icons.groups),
-                text: 'Communities',
-              ),
-              Tab(
-                icon: Icon(Icons.call),
-                text: 'Calls',
-              ),
+            tabs: [
+              _buildTab("assets/svgs/chats.svg", "Chats", 0),
+              _buildTab("assets/svgs/groups.svg", "Communities", 1),
+              _buildTab("assets/svgs/call.svg", "Calls", 2),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget _buildTab(String assetPath, String text, int index) {
+    return Tab(
+      icon: Builder(
+        builder: (context) {
+          final isSelected = DefaultTabController.of(context)?.index == index;
+          final color =
+              isSelected ? Theme.of(context).colorScheme.primary : Colors.grey;
+          return SvgPicture.asset(
+            assetPath,
+            height: 24,
+            width: 24,
+            colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+          );
+        },
+      ),
+      text: text,
     );
   }
 }
