92b4a31a0e4f82426886211c2e4f7050a5bcbbd4
Mohit Lahoti <engtechno25@gmail.com>
2025-02-11 00:36:15 +0530

    Add isPin, isMute and unread in message model

--- lib/home/models/messages.dart
+++ lib/home/models/messages.dart
@@ -2,12 +2,18 @@ class Message {
   final String username;
   final String lastMessage;
   final String timestamp;
+  final bool? isPin;
+  final bool? isMute;
+  final int? unread;
   final String profileImage;
 
   Message({
     required this.username,
     required this.lastMessage,
     required this.timestamp,
+    this.isMute,
+    this.isPin,
+    this.unread,
     required this.profileImage,
   });
 }
