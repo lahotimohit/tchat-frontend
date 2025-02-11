850ce0a85e5af6dd9a5f3f02fcae534347f841b2
Mohit Lahoti <engtechno25@gmail.com>
2025-02-11 17:06:39 +0530

    Add softwrap, overflow and maxlines in customtext widget

--- lib/src/widgets/custom_text.dart
+++ lib/src/widgets/custom_text.dart
@@ -6,15 +6,22 @@ class CustomText extends StatelessWidget {
   final String? text;
   final double? size;
   final Color? color;
+  final TextOverflow? overflow;
   final FontWeight? weight;
   final TextAlign? align;
+  final int? maxlines;
+  final bool? isSoftWrap;
   final double? letterspacing;
   final Alignment? alignment;
+
   const CustomText(
       {super.key,
       @required this.text,
       this.size,
+      this.maxlines,
       this.color,
+      this.isSoftWrap,
+      this.overflow,
       this.weight,
       this.alignment,
       this.letterspacing,
@@ -26,9 +33,12 @@ class CustomText extends StatelessWidget {
       alignment: alignment ?? Alignment.topLeft,
       child: Text(text!,
           textAlign: align ?? TextAlign.left,
+          overflow: overflow,
+          softWrap: isSoftWrap,
+          maxLines: maxlines,
           style: GoogleFonts.poppins(
               fontSize: size ?? 16,
-              letterSpacing: letterspacing,
+              letterSpacing: letterspacing ?? 0.1,
               color: color ?? black,
               fontWeight: weight ?? FontWeight.normal)),
     );
