ec80ee43251a21e5f483a9bfae271a062eef454e
Mohit Lahoti <engtechno25@gmail.com>
2025-02-12 17:50:15 +0530

    Remove previous setting aspects from project

--- lib/setting/models/profile_option.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ProfileOption {
-  final IconData icon;
-  final String title;
-  final String subtitle;
-
-  ProfileOption({required this.icon, required this.title, required this.subtitle});
-}
