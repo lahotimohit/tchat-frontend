411a47e4d5806eb337cb537a7fd3dce69fb82d32
Mohit Lahoti <engtechno25@gmail.com>
2025-02-23 18:55:35 +0530

    Write api logic for registration except profile photo uploading

--- lib/src/api/register.dart
+++ lib/src/api/register.dart
@@ -1,25 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:tchat_frontend/api/register.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tchat_frontend/generated_api/client_index.dart';
+import 'package:tchat_frontend/generated_api/tchat.models.swagger.dart';
+import 'package:tchat_frontend/src/providers/storage.dart';
 import 'package:tchat_frontend/src/widgets/snackmessage.dart';
-import 'package:tchat_frontend/src/screens/dashboard.dart';
 
 Future<bool> onRegister(BuildContext context, String name, String about) async {
+  SecureStorage storage = SecureStorage();
   try {
-    RegisterAPI api = RegisterAPI();
-    final Map<String, dynamic> result =
-        await api.dioRegister("https://placehold.co/600x400", about, name);
-
-    if (result['code'] == 401) {
-      snackmessage(context, result['error']);
-      return false;
-    } else {
-      snackmessage(context, result['msg']);
-      Navigator.of(context)
-          .push(MaterialPageRoute(builder: (ctx) => const HomeMainScreen()));
+    String accessToken ="";
+    String refreshToken ="";
+    await dotenv.load(fileName: '.env');
+    await storage.readData("accessToken").then((value) {
+        accessToken = value;
+      });
+    await storage.readData("refreshToken").then((value) {
+        refreshToken = value;
+      });
+    late Tchat tchatClient = Tchat.create(baseUrl: Uri.parse(dotenv.env['SERVER_URL']!));
+    final registerDto = RegisterDto(name: name, about: about);
+    final response = await tchatClient.authRegisterPost(body: registerDto, authorization: "Bearer $accessToken", refresh: refreshToken);
+    if(response.isSuccessful) {
+      final newAccessToken = response.body["tokens"]["accessToken"];
+      final newRefreshToken = response.body["tokens"]["accessToken"];
+      storage.writeSecureData("accessToken", newAccessToken);
+      storage.writeSecureData("refreshToken", newRefreshToken);
       return true;
     }
+    else{
+      final errorResponse = json.decode(response.error.toString());
+      context.mounted? snackmessage(context, errorResponse['message']) : null;
+      return false;
+    }
   } catch (e) {
-    snackmessage(context, "Internal Server Error");
+    context.mounted ? snackmessage(context, "Error $e"): null;
     return false;
   }
 }
