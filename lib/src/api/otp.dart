91f93a4b9840e34d0ab8d7101e8efb5081780183
Mohit Lahoti <engtechno25@gmail.com>
2025-02-23 18:53:32 +0530

    Write api logic for otp through swagger

--- lib/src/api/otp.dart
+++ lib/src/api/otp.dart
@@ -1,40 +1,50 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:tchat_frontend/api/otp.dart';
-import 'package:tchat_frontend/src/screens/dashboard.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:tchat_frontend/generated_api/client_index.dart';
+import 'package:tchat_frontend/generated_api/tchat.models.swagger.dart';
+import 'package:tchat_frontend/src/animations/fade_pageroute.dart';
 import 'package:tchat_frontend/src/providers/storage.dart';
+import 'package:tchat_frontend/src/screens/dashboard.dart';
 import 'package:tchat_frontend/src/screens/register.dart';
+import 'package:tchat_frontend/src/widgets/snackmessage.dart';
 
 Future<bool> onVerifyOTP(BuildContext context, String otp) async {
   SecureStorage storage = SecureStorage();
-
+  await dotenv.load(fileName: '.env');
+  late Tchat tchatClient = Tchat.create(baseUrl: Uri.parse(dotenv.env['SERVER_URL']!));
   try {
-    OtpAPI api = OtpAPI();
-    final Map<String, dynamic> result = await api.verifyOtp(otp);
-
-    if (result['code'] == 200) {
-      String isRegistered = "";
-      await storage.readData("isRegistered").then((value) {
-        isRegistered = value;
+    String otpToken="";
+    await storage.readData("otpToken").then((value) {
+        otpToken = value;
       });
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(result['msg'])));
-      isRegistered == "true"
-          ? Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (ctx) => const HomeMainScreen()),
-              (Route<dynamic> route) => false)
-          : Navigator.of(context).pushAndRemoveUntil(
-              MaterialPageRoute(builder: (ctx) => const SignupScreen()),
-              (Route<dynamic> route) => false);
+    final otpDto = VerifyOtpDto(otp: otp);
+    final response = await tchatClient.authVerifyOtpPost(otpToken: otpToken, body: otpDto);
+    Navigator.of(context).pop();
+    if(response.isSuccessful) {
+      final String accessToken = response.body['tokens']['accessToken'];
+      final sessionResponse = await tchatClient.authSessionGet(authorization: "Bearer $accessToken");
+      if(sessionResponse.isSuccessful) {
+        final bool isRegistered = sessionResponse.body['session']['isRegistered'];
+        if(!isRegistered) {
+          Navigator.of(context).pushReplacement(fadeRoute(const SignupScreen()));
+        }
+        else {
+          Navigator.of(context).pushReplacement(fadeRoute(const HomeMainScreen()));
+        }
+        storage.writeSecureData("accessToken", accessToken);
+        storage.writeSecureData("refreshToken", response.body['tokens']['refreshToken']);
+        storage.deleteData("otpToken");
+      }
       return true;
-    } else {
-      ScaffoldMessenger.of(context)
-          .showSnackBar(SnackBar(content: Text(result['error'])));
+    }
+    else {
+      final jsonError = json.decode(response.error.toString());
+      context.mounted? snackmessage(context, jsonError['message']) : null;
       return false;
     }
   } catch (e) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("An error occurred: $e")),
-    );
+    context.mounted? snackmessage(context, "An error occurred: $e") : null;
     return false;
   }
 }
