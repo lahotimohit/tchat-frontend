845c7f2ee583d0060895b463ef99dddbd5c961fd
Mohit Lahoti <engtechno25@gmail.com>
2025-03-04 20:06:27 +0530

    Modify contacts from native device screen

--- lib/home/widget/all_contacts.dart
+++ lib/home/widget/all_contacts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:tchat_frontend/src/common.dart';
+import 'package:tchat_frontend/src/widgets/app_bar_dashboard.dart';
 import 'package:tchat_frontend/src/widgets/custom_text.dart';
 
 class ContactsScreen extends StatefulWidget {
@@ -38,57 +39,40 @@ class _ContactsScreenState extends State<ContactsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.primary,
-        leading: IconButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-        ),
-        title: Column(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const CustomText(text: "Select Contact", color: white, size: 14,),
-            CustomText(text: "${_contacts.length} contacts", color: white, size: 10,)
+            CustomAppBar(searchHint: "Search among ${_contacts.length} contacts", tab: ""),
+            const Padding(
+              padding: EdgeInsets.only(left: 18, top: 8, bottom: 8),
+              child: CustomText(text: "Contacts on TChat", color: grey, size: 14, weight: FontWeight.w500,)
+            ),
+            Expanded(
+              child: _contacts.isEmpty
+                  ? const Center(
+                      child: CustomText(text: "No contacts available", color: grey, alignment: Alignment.center,)
+                    )
+                  : Scrollbar(
+                      child: ListView.builder(
+                        itemCount: _contacts.length,
+                        itemBuilder: (context, index) {
+                          Contact contact = _contacts[index];
+                          return ListTile(
+                            leading:
+                                CircleAvatar(child: Image.asset('assets/images/user.jpg')),
+                            title: CustomText(text: contact.displayName, weight: FontWeight.w500, overflow: TextOverflow.ellipsis,),
+                            subtitle: CustomText(text: contact.phones.isNotEmpty
+                                ? contact.phones.first.number
+                                : 'No Phone Number', size: 13, color: grey,),
+                          );
+                        },
+                      ),
+                    ),
+            ),
           ],
         ),
-        actions: [
-          IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.more_vert, color: Colors.white70)),
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.only(left: 18, top: 8, bottom: 8),
-            child: CustomText(text: "Contacts on TChat", color: grey, size: 14, weight: FontWeight.w500,)
-          ),
-          Expanded(
-            child: _contacts.isEmpty
-                ? const Center(
-                    child: CustomText(text: "No contacts available", color: grey)
-                  )
-                : Scrollbar(
-                    child: ListView.builder(
-                      itemCount: _contacts.length,
-                      itemBuilder: (context, index) {
-                        Contact contact = _contacts[index];
-                        return ListTile(
-                          leading:
-                              const CircleAvatar(child: Icon(Icons.person)),
-                          title: Text(contact.displayName),
-                          subtitle: Text(contact.phones.isNotEmpty
-                              ? contact.phones.first.number
-                              : 'No Phone Number'),
-                        );
-                      },
-                    ),
-                  ),
-          ),
-        ],
       ),
     );
   }
